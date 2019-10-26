affd5fd2bdae3545be1a1200ed6cf7257fcb3f30
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-10-25 23:08:22 -0400

    Updates

---
+++ precache-manifest.ab36aee1eb34d818181bdea49ed390d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85923954047073e3f97401acd1c27bcc",
+    "url": "/LearnSoftwareDevelopment/index.html"
+  },
+  {
+    "revision": "28a4d92707b4bda634cd",
+    "url": "/LearnSoftwareDevelopment/static/css/main.645cf225.chunk.css"
+  },
+  {
+    "revision": "4014d4a42e601861db28",
+    "url": "/LearnSoftwareDevelopment/static/js/2.fb297438.chunk.js"
+  },
+  {
+    "revision": "28a4d92707b4bda634cd",
+    "url": "/LearnSoftwareDevelopment/static/js/main.bd4ba92f.chunk.js"
+  },
+  {
+    "revision": "38031832cf4eaea506be",
+    "url": "/LearnSoftwareDevelopment/static/js/runtime-main.96aaec5c.js"
+  }
+]);
